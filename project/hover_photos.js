64c1dd40d23f90d52242fa4245ff6e0ead6384f0
IPogorelova <pisseversk@mail.ru>
2018-09-05 16:27:10 +0300

    animation of text hide/show edited

--- project/hover_photos.js
+++ project/hover_photos.js
@@ -1,54 +1,59 @@
 $(document).ready(function() {
 
-        $("#photo1").mouseover(function () {
-            if ($(window).width() >= 768) {
-                $("#general_text").hide('1s');
-                $("#hover_project1").show('1s');
-            }
-        });
-        $(this).mouseout(function () {
-            if ($(window).width() >= 768) {
-                $("#hover_project1").hide('0.5s');
-                $("#general_text").show('0.5s');
-            }
-        });
+    $("#photo_content").mouseover(function () {
+        if ($(window).width() >= 768) {
+            $("#general_text").hide('1s');
+        }
+    });
 
-        $("#photo2").mouseover(function () {
-            if ($(window).width() >= 768) {
-                $("#general_text").hide('0.5s');
-                $("#hover_project2").show('0.5s');
-            }
-        });
-        $(this).mouseout(function () {
-            if ($(window).width() >= 768) {
-                $("#hover_project2").hide('0.5s');
-                $("#general_text").show('0.5s');
-            }
-        });
+    $(".column.short").mouseover(function () {
+        if ($(window).width() >= 768) {
+            $("#general_text").show('0.5s');
+        }
+    });
 
-        $("#photo3").mouseover(function () {
-            if ($(window).width() >= 768) {
-                $("#general_text").hide('0.5s');
-                $("#hover_project3").show('0.5s');
-            }
-        });
-        $(this).mouseout(function () {
-            if ($(window).width() >= 768) {
-                $("#hover_project3").hide('0.5s');
-                $("#general_text").show('0.5s');
-            }
-        });
 
-        $("#photo4").mouseover(function () {
-            if ($(window).width() >= 768) {
-                $("#general_text").hide('0.5s');
-                $("#hover_project4").show('0.5s');
-            }
-        });
-        $(this).mouseout(function () {
-            if ($(window).width() >= 768) {
-                $("#hover_project4").hide('0.5s');
-                $("#general_text").show('0.5s');
-            }
-        });
+    $("#photo1").mouseover(function () {
+        if ($(window).width() >= 768) {
+            $("#hover_project1").show('1s');
+        }
+    });
+    $(this).mouseout(function () {
+        if ($(window).width() >= 768) {
+            $("#hover_project1").hide('0.5s');
+        }
+    });
+
+    $("#photo2").mouseover(function () {
+        if ($(window).width() >= 768) {
+            $("#hover_project2").show('0.5s');
+        }
+    });
+    $(this).mouseout(function () {
+        if ($(window).width() >= 768) {
+            $("#hover_project2").hide('0.5s');
+        }
+    });
+
+    $("#photo3").mouseover(function () {
+        if ($(window).width() >= 768) {
+            $("#hover_project3").show('0.5s');
+        }
+    });
+    $(this).mouseout(function () {
+        if ($(window).width() >= 768) {
+            $("#hover_project3").hide('0.5s');
+        }
+    });
+
+    $("#photo4").mouseover(function () {
+        if ($(window).width() >= 768) {
+            $("#hover_project4").show('0.5s');
+        }
+    });
+    $(this).mouseout(function () {
+        if ($(window).width() >= 768) {
+            $("#hover_project4").hide('0.5s');
+        }
+    });
 });
--- project/scroll.js
+++ project/scroll.js
@@ -6,21 +6,23 @@
     });
 });
 */
-/*
+
 var photo1 = document.getElementById('photo1');
 var about_text = document.getElementById('general_text');
 /*var content = document.getElementsByClassName('column.long col_content');*/
 
-/*window.onscroll = function() {
+window.onscroll = function() {
     var coords = photo1.getBoundingClientRect();
 
     if (coords.top < 0) {
         alert('here i am :3');
-        about_text.classList.add('scrolled');
+        /*about_text.classList.add('scrolled');*/
+        about_text.getPropertyValue('display: none');
     }
     if (coords.top >= 0) {
-        about_text.classList.remove('scrolled');
+        /*about_text.classList.remove('scrolled');*/
+        about_text.getPropertyValue('display: block');
     }
 
 };
-*/
+
