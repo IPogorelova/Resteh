6d993eb308872adf2ca1f9cf383a14f2bb8e53da
IPogorelova <pisseversk@mail.ru>
2018-09-17 18:09:20 +0300

    some mobile version edits are done and jQuery animation of text is changed

--- project/hover_photos.js
+++ project/hover_photos.js
@@ -2,58 +2,58 @@ $(document).ready(function() {
 
     $("#photo_content").mouseover(function () {
         if ($(window).width() >= 768) {
-            $("#general_text").hide('1s');
+            $("#general_text").fadeOut('700');
         }
     });
 
     $(".column.short").mouseover(function () {
         if ($(window).width() >= 768) {
-            $("#general_text").show('0.5s');
+            $("#general_text").fadeIn('700');
         }
     });
 
 
     $("#photo1").mouseover(function () {
         if ($(window).width() >= 768) {
-            $("#hover_project1").show('1s');
+            $("#hover_project1").stop(true, true).fadeIn('700');
         }
     });
     $(this).mouseout(function () {
         if ($(window).width() >= 768) {
-            $("#hover_project1").hide('0.5s');
+            $("#hover_project1").stop(true, true).fadeOut('700');
         }
     });
 
     $("#photo2").mouseover(function () {
         if ($(window).width() >= 768) {
-            $("#hover_project2").show('0.5s');
+            $("#hover_project2").stop(true, true).fadeIn('700');
         }
     });
     $(this).mouseout(function () {
         if ($(window).width() >= 768) {
-            $("#hover_project2").hide('0.5s');
+            $("#hover_project2").stop(true, true).fadeOut('700');
         }
     });
 
     $("#photo3").mouseover(function () {
         if ($(window).width() >= 768) {
-            $("#hover_project3").show('0.5s');
+            $("#hover_project3").stop(true, true).fadeIn('700');
         }
     });
     $(this).mouseout(function () {
         if ($(window).width() >= 768) {
-            $("#hover_project3").hide('0.5s');
+            $("#hover_project3").stop(true, true).stop().fadeOut('700');
         }
     });
 
     $("#photo4").mouseover(function () {
         if ($(window).width() >= 768) {
-            $("#hover_project4").show('0.5s');
+            $("#hover_project4").stop(true, true).fadeIn('0.5s');
         }
     });
     $(this).mouseout(function () {
         if ($(window).width() >= 768) {
-            $("#hover_project4").hide('0.5s');
+            $("#hover_project4").stop(true, true).fadeOut('0.5s');
         }
     });
 });
